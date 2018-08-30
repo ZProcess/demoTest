92b06f8a97fb748004626dfc8cbec72052668683
zhangjingcheng <zhangjingcheng>
2018-08-31 01:55:29 +0800

    2018.8.31

---
+++ rabbitMQ/rabbitMQ_Reciver/src/main/java/com/HelloSender.java
@@ -0,0 +1,21 @@
+package com;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class HelloSender {
+    @Autowired
+    private AmqpTemplate rabbitTemplate;
+
+    public void send() {
+        String context = "hello zjc " + new Date();
+        System.out.println("Sender $$$$: " + context);
+        this.rabbitTemplate.convertAndSend("queue", context);
+
+    }
+
+}
