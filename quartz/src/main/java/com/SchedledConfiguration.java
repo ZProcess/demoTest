0b32edb9cffa9dc925f250385c1fcaae8da69e52
zhangjingcheng <zhangjingcheng>
2018-08-31 02:23:02 +0800

    2018.8.31

--- quartz/src/main/java/com/SchedledConfiguration.java
+++ quartz/src/main/java/com/SchedledConfiguration.java
@@ -1,18 +1,22 @@
 package com;
 
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
 @Configuration
+@Component
+@EnableScheduling
 public class SchedledConfiguration {
+
     @Scheduled(cron = "0 0/1 * * * ?") // 每分钟执行一次
     public void work() throws Exception {
         System.out.println("执行调度任务："+new Date());
     }
-
 
     @Scheduled(fixedRate = 5000)//每5秒执行一次
     public void play() throws Exception {
@@ -20,13 +24,10 @@ public class SchedledConfiguration {
     }
 
 
-
     @Scheduled(cron = "0/2 * * * * ?") //每2秒执行一次
     public void doSomething() throws Exception {
         System.out.println("每2秒执行一个的定时任务："+new Date());
     }
-
-
 
 
     @Scheduled(cron = "0 0 0/1 * * ? ") // 每一小时执行一次
