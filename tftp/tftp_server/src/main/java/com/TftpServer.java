e388c8e20a70aa6deed4363fbba53332bc9beead
ZProcess <fjqzzjc/778899654>
2018-09-17 00:32:42 +0800

    Merge branch 'master' of https://github.com/ZProcess/demoTest
    # Conflicts:
    #	.idea/compiler.xml
    #	.idea/encodings.xml
    #	.idea/misc.xml
    #	.idea/modules.xml

--- tftp/tftp_server/src/main/java/com/TftpServer.java
+++ tftp/tftp_server/src/main/java/com/TftpServer.java
@@ -7,7 +7,11 @@ public class TftpServer {
     private static final int PORT=69;
     public static void main(String[] args) throws SocketException, UnsupportedEncodingException {
         UdpServer udpServer=new UdpServer(PORT);
+        byte[] b1=new byte[2];
+        b1[1]=1;
+        byte[] b2="haha".getBytes("utf-8");
         UdpTransport udpTransport=new UdpTransport();
+        udpTransport.assemblePackage(b1,b2);
 
     }
 
--- tftp/tftp_server/src/main/java/com/UdpTransport.java
+++ tftp/tftp_server/src/main/java/com/UdpTransport.java
@@ -1,22 +1,25 @@
 package com;
 
-import com.sun.org.apache.xpath.internal.operations.String;
+
 
 import java.io.UnsupportedEncodingException;
 import java.net.DatagramPacket;
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class UdpTransport {
 
      private static final int DATA_SIZE=512; //数据长的
+     private static final int DATA_BUFFER=1024;
      private byte[] temp;//用于组装数据
      private byte[] data;
      private byte[] buffer;
      private DatagramPacket datagramPacket;
     UdpTransport(){
         data=new byte[DATA_SIZE];
-
+        buffer=new byte[DATA_BUFFER];
     }
     public void sendRRQ(){
 
@@ -35,8 +38,22 @@ public class UdpTransport {
     public void sendAck(){
 
     }
-    public void assemblePackage(String...temp) throws UnsupportedEncodingException {
+    public void assemblePackage(byte[]...temp) throws UnsupportedEncodingException {
+        int numb=temp.length;
+        int len=0;
+        for(int i=0;i<numb;i++){
+            for(int j=0;j<temp[i].length;j++){
+                buffer[len]=temp[i][j];
+                len++;
+            }
+        }
+        byte[] t= Arrays.copyOf(buffer,len);
+        System.out.println(t.length);
+        byte[] t2=new byte[512];
+        System.arraycopy(t,2,t2,0,len-2);
+        System.out.println( new String(t2,"UTF-8"));
 
     }
+
 
 }
