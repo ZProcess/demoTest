67e114011cd6e8c0775cce262f35dda03597dd48
zhangjingcheng <zhangjingcheng>
2018-09-18 08:28:52 +0800

    2018.9.3 udp

--- tftp/tftp_server/src/main/java/com/HandleServer.java
+++ tftp/tftp_server/src/main/java/com/HandleServer.java
@@ -2,12 +2,24 @@ package com;
 
 public class HandleServer {
 
-    public void dealRequest() {
-
+    public void dealRequest(byte[] pack) {
+       int code=(int)pack[1];
+       if(code==1){
+           String fileName="";
+            this.dealRRQ(fileName);
+       }else if(code==2){
+            this.dealWRQ();
+       }else if(code==3){
+            this.dealData();
+       }else if(code==4){
+            this.dealAck();
+       }else if(code==5){
+            this.dealError();
+       }
 
     }
 
-    public void dealRRQ() {
+    public void dealRRQ(String fileName) {
 
     }
 
